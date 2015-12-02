c156609d2c115f9a121fb121f6f335873b94a72f
Gaoyuan Chen <289312802@qq.com>
2015-12-02 15:54:04 -0600

    first commit with Parse

---
+++ HooferSNS/HooferSNS/HooferSNS-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HooferSNS-Bridging-Header.h
+//  HooferSNS
+//
+//  Created by Gaoyuan Chen on 12/2/15.
+//  Copyright © 2015 Gaoyuan Chen. All rights reserved.
+//
+
+#ifndef HooferSNS_Bridging_Header_h
+#define HooferSNS_Bridging_Header_h
+#import <Parse/Parse.h>
+
+
+#endif /* HooferSNS_Bridging_Header_h */
---
+++ HooferSNS/HooferSNS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HooferSNS
+//
+//  Created by Gaoyuan Chen on 12/2/15.
+//  Copyright © 2015 Gaoyuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
