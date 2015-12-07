1e64ee7da85537a62b24a04147020f1649c8456c
Gaoyuan Chen <289312802@qq.com>
2015-12-06 18:45:30 -0600

    Add alert view for StatsViewController for signing in

--- HooferSNS/HooferSNS/StatsViewController.swift
+++ HooferSNS/HooferSNS/StatsViewController.swift
@@ -18,7 +18,20 @@ class StatsViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         if (PFUser.currentUser() == nil) {
-            self.performSegueWithIdentifier("signInFromStats", sender: self)
+            
+            let alert = UIAlertController(title: "Sign in?", message: "You have to sign in to continue this page", preferredStyle: .Alert)
+            let firstAction = UIAlertAction(title: "Cancel", style: .Default) { (alert: UIAlertAction!) -> Void in
+                self.tabBarController?.selectedIndex = 0;
+            }
+            
+            let secondAction = UIAlertAction(title: "Sign In", style: .Default) { (alert: UIAlertAction!) -> Void in
+                self.performSegueWithIdentifier("signInFromStats", sender: self)
+            }
+            
+            alert.addAction(firstAction)
+            alert.addAction(secondAction)
+            presentViewController(alert, animated: true, completion:nil)
+        
         }
     }
     
